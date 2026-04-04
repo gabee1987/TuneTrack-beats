7897aefe2581ae5762fc06eef0571f716e713622
Gabor Koncz <gabee1987@gmail.com>
2026-04-04 09:17:35 +0200

    Remove generated web build files

--- apps/web/src/app/App.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-export function App() {
-    return _jsx(RouterProvider, { router: router });
-}
--- apps/web/src/services/socket/socketClient.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-const serverUrl = import.meta.env.VITE_SERVER_URL ?? "http://localhost:3001";
-export const socketClient = io(serverUrl, {
-    autoConnect: false,
-});
